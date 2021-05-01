a0a8d15d520f819d37519218f07a22151f38b7be
Ayo Tosan Samuel <46663234+Stosan@users.noreply.github.com>
2021-05-01 19:40:14 +0100

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+  //dropdown
+   // Drop down menu on mobile screens
+   $(".mob-nav").click(function() {
+    if ($(".dropDN-menu").css("display") === "none") {
+      $(".dropDN-menu").css("display", "block");
+    } else {
+      $(".dropDN-menu").css("display", "none");
+    }
+  });
+  $(".dropDN-menu a").click(function() {
+    $(".dropDN-menu").css("display","none");
+  });
+//shadow function
   $(window).scroll(function() {
       var scroll = $(window).scrollTop();
       if (scroll > 30) {
@@ -7,4 +21,14 @@ $(document).ready(function() {
         $("#nav").removeClass("head-shadow");
       }
   })
+
+  //menubar
+   // effect for portfolio section
+   $(".item").mouseenter(function() {
+    $(".color").css("display","none");
+    $(this).find(".color").css("display","block");
+  });
+  $(".item").mouseleave(function() {
+    $(".color").css("display","block");
+  });
 })
